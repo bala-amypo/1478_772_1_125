05911fb5829b6a2eccf862afc26d02338cec3713
bala-amypo <bala@amypo.in>
2025-12-26 17:38:47 +0000

    User commit

--- src/main/java/com/example/demo/repository/ProfitCalculationRecordRepository.java
+++ src/main/java/com/example/demo/repository/ProfitCalculationRecordRepository.java
@@ -2,12 +2,16 @@ package com.example.demo.repository;
 
 import com.example.demo.entity.ProfitCalculationRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface ProfitCalculationRecordRepository extends JpaRepository<ProfitCalculationRecord, Long> {
+public interface ProfitCalculationRecordRepository extends 
+    JpaRepository<ProfitCalculationRecord, Long>, 
+    JpaSpecificationExecutor<ProfitCalculationRecord> {
+    
     List<ProfitCalculationRecord> findByMenuItemId(Long menuItemId);
     List<ProfitCalculationRecord> findByProfitMarginGreaterThanEqual(Double margin);
 }
